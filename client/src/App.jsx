02c67c3cd4d218014f6be5eed29405473a14a85c
shubhamdeveloper188 <krguptashubham56@gmail.com>
2024-05-19 20:57:44 +0530

    designed signup page ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,15 +7,14 @@ import Profile from "./pages/Profile"
 import Header from './components/Header';
 
 export default function App() {
-  return ( 
-  <BrowserRouter>
+  return ( <BrowserRouter>
   <Header />
   <Routes>
-    < Route path="/" element={<Home/>} />
-    < Route path="/" element={<SignIn/>} />
-    < Route path="/" element={<SignUp/>} />
-    < Route path="/" element={<About/>} />
-    < Route path="/" element={<Profile/>} />
+    < Route path="/" element={<Home />} />
+    < Route path="/sign-in" element={<SignIn />} />
+    < Route path="/sign-up" element={<SignUp />} />
+    < Route path="/about" element={<About />} />
+    < Route path="/profile" element={<Profile />} />
 
   </Routes>
   </BrowserRouter>
