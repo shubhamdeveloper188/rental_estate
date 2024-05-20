5690bc8afdfe4528e877beb747ebf797db689980
shubhamdeveloper188 <krguptashubham56@gmail.com>
2024-05-20 21:41:53 +0530

    add redux toolkit

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -6,10 +6,10 @@ import jwt from 'jsonwebtoken';
 export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
     const hashedPassword = bcryptjs.hashSync(password, 10)
-    const newUser = new User ({ username, email, password: hashedPassword});
+    const newUser = new User ({ username, email, password: hashedPassword });
     try {
-        await newUser.save()
-    res.status(201).json('User crated succesfully!')
+        await newUser.save();
+        res.status(201).json('User crated succesfully!')
 
     } catch (error) {
         next(error);
@@ -17,20 +17,18 @@ export const signup = async (req, res, next) => {
     
 };
 
-
-export const signin = async(req, res, next) => {
-    const {email, password } = req.body;
+export const signin = async (req, res, next) => {
+    const { email, password } = req.body;
     try{
         const validUser = await User.findOne({ email });
-        if(!validUser) return next (errorHandler(404, 'User not found!'));
-        const validPassword =bcryptjs.compareSync(password, validUser.password);
-        if(!validPassword) return next (errorHandler(401, 'Wrong credentials!'));
+        if (!validUser) return next ( errorHandler(404, 'User not found!'));
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if (!validPassword) return next(errorHandler(401, 'Wrong credentials!'));
         const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET)
         const { password: pass, ...rest } = validUser._doc;
-        res
-            .cookie('access_token', token, { httpOnly: true })
+        res.cookie('access_token', token, { httpOnly: true })
             .status(200)
-            .json(validUser);
+            .json(rest);
     }   catch (error) {
         next(error);
     }
--- api/models/user.model.js
+++ api/models/user.model.js
@@ -16,7 +16,9 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
     }
-}, {timestamps: true});
+}, 
+ { timestamps: true }
+);
 
 const User = mongoose.model('User', userSchema);
 
