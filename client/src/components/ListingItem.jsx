e564145e34c9f0d7e64c3a0885cf5b2bc6764592
shubhamdeveloper188 <krguptashubham56@gmail.com>
2024-06-05 00:29:56 +0530

    Completed Home Page UI

--- client/src/components/ListingItem.jsx
+++ client/src/components/ListingItem.jsx
@@ -6,7 +6,7 @@ export default function ListingItem({ listing }) {
     <div className='bg-white shadow-md hover:shadow-lg transition-shadow overflow-hidden rounded-lg w-full sm:w-[330px]'>
       <Link to={`/listing/${listing._id}`}>
         <img
-          src={
+          src={ 
             listing.imageUrls[0] ||
             'https://53.fs1.hubspotusercontent-na1.net/hub/53/hubfs/Sales_Blog/real-estate-business-compressor.jpg?width=595&height=400&name=real-estate-business-compressor.jpg'
           }
